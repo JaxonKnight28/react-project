c457b1edb28d59d8a3d52ee999324698b30727ac
JaxonKnight28 <jaxon.knight2725@stu.mtec.edu>
2022-01-11 09:44:07 -0700

    added starting files

---
+++ space-search/src/App.tsx
@@ -0,0 +1,12 @@
+import React, { useEffect, useState } from 'react';
+import logo from './logo.svg';
+import './styles/App.css';
+
+
+function App() {
+  return (
+    <div>This is a test</div>
+  )
+}
+
+export default App;
